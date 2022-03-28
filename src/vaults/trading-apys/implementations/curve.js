b23905c731f866084a49ff7ef07c4d3b5aa5e205
Corey <corey@dolomite.io>
2022-03-28 19:56:15 -0400

    added Arbitrum CRV trading APY function

---
+++ src/vaults/trading-apys/implementations/curve.js
@@ -0,0 +1,22 @@
+const BigNumber = require('bignumber.js')
+const { get } = require('lodash')
+const { cachedAxios } = require('../../../lib/db/models/cache.js')
+
+const getTradingApy = async poolId => {
+  let apy
+
+  try {
+    const response = await cachedAxios.get('https://stats.curve.fi/raw-stats-arbitrum/apys.json')
+
+    apy = new BigNumber(get(response, `data.apy.day[${poolId}]`, 0))
+  } catch (err) {
+    console.error('convex API error: ', err)
+    apy = new BigNumber(0)
+  }
+
+  return apy.isNaN() ? '0' : apy.toFixed(2, 1)
+}
+
+module.exports = {
+  getTradingApy,
+}
